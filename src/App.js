0088551a3e5e7faa9f903086742bc14408bb34d5
Detelina <palova.detelina@gmail.com>
2021-11-24 18:02:18 +0200

    add app title

--- src/App.js
+++ src/App.js
@@ -58,13 +58,13 @@ function App(props) {
 
   return (
     <div className="App">
+       <h1>Calendar</h1>
       {flagValue &&
         <div>
-          <h1>Calendar</h1>
           <h2>Add New Event</h2>
           <div>
             <input type="text" placeholder="Add Title" style={{ width: "20%", marginRight: "10px" }} value={newEvent.title} onChange={(e) => setNewEvent({ ...newEvent, title: e.target.value })} />
-            <DatePicker placeholderText="Start Date" style={{ marginRight: "10px" }} selected={newEvent.start} onChange={(start) => setNewEvent({ ...newEvent, start })} />
+            <DatePicker placeholderText="Start Date" selected={newEvent.start} onChange={(start) => setNewEvent({ ...newEvent, start })} />
             <DatePicker placeholderText="End Date" selected={newEvent.end} onChange={(end) => setNewEvent({ ...newEvent, end })} />
             <button stlye={{ marginTop: "10px" }} onClick={handleAddEvent}>
               Add Event
